9af6f32526ca8a1f318adf30c7ea1a4d1553c85d
quinwhit470 <quinwhit470@SDS-L-2BHDF63>
2024-09-11 11:18:53 -0400

    Add project files.

---
+++ PlayGame/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace PlayGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Error = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Error
+            // 
+            this.Error.BackColor = System.Drawing.Color.Orange;
+            this.Error.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Error.Location = new System.Drawing.Point(267, 97);
+            this.Error.Name = "Error";
+            this.Error.Size = new System.Drawing.Size(265, 125);
+            this.Error.TabIndex = 4;
+            this.Error.Text = "Error";
+            this.Error.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Error.UseCompatibleTextRendering = true;
+            this.Error.Click += new System.EventHandler(this.Error_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Orange;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Error);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Error;
+    }
+}
+
---
+++ PlayGame/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Media;
+
+namespace PlayGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Error_Click(object sender, EventArgs e)
+        {
+
+            SoundPlayer sp = new SoundPlayer(Properties.Resources.TV_Static_Mike_Koenig_1287909192);
+            sp.Play();
+
+            Error.Text = "3";
+            Refresh();
+            Thread.Sleep(1000);
+
+            Error.Text = "2";
+            Refresh();
+            Thread.Sleep(1000);
+
+            Error.Text = "1";
+            Refresh();
+            Thread.Sleep(1000);
+
+            Error.Text = "Go!";
+            Refresh();
+            Thread.Sleep(1000);
+            
+           
+
+
+
+
+
+
+        }
+    }
+}
